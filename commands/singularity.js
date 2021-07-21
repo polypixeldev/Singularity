a3ec2c37cce7d49d2369e671000b4a2867b3b7e5
Poly-Pixel <sammelofer@outlook.com>
2021-07-20 20:17:25 -0400

    Add shop functionality to My Singularity!

--- commands/singularity.js
+++ commands/singularity.js
@@ -67,7 +67,7 @@ module.exports = {
 						.setDescription(`Congrats! Your Singularity is now size \`${userMS.singularity.size}\`!`);
 
 						return msg.channel.send(embed);
-					})
+					});
 				}
 			})
 			.catch(() => {
@@ -77,14 +77,157 @@ module.exports = {
 				return msg.channel.send(embed);
 			});
 
+		} else if(args[0] === 'shop'){
+			const items = {
+				tiny_trophy: {
+					protons: 1000000,
+					electrons: 500000,
+					darkMatter: 0
+				},
+				trophy: {
+					protons: 10000000,
+					electrons: 1000000,
+					darkMatter: 1
+				},
+				giant_trophy: {
+					protons: 1000000000,
+					electrons: 10000000,
+					darkMatter: 5
+				}
+			}
+
+			if(args[1] === 'buy'){
+				let userMS = await client.utils.loadMsInfo(serverDoc, msg.author.id);
+
+				if(!items.hasOwnProperty(args[2])){ //eslint-disable-line no-prototype-builtins
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription('That is not a valid item!');
+
+					return msg.channel.send(embed);
+				}
+				if(!args[3]) args[3] = 1;
+
+				if(userMS.protons >= items[args[2]].protons * args[3] && userMS.electrons >= items[args[2]].electrons * args[3] && userMS.darkMatter >= items[args[2]].darkMatter * args[3]){
+					userMS.protons -= items[args[2]].protons;
+					userMS.electrons -= items[args[2]].electrons;
+					userMS.darkMatter -= items[args[2]].darkMatter;
+					userMS.items.push(args[2]);
+
+					serverDoc.markModified('ms');
+					serverDoc.save().then(() => {
+						const embed = new Discord.MessageEmbed()
+						.setColor(0x000000)
+						.setDescription(`
+							Purchase completed!
+
+							\t + **${args[3]}** ${args[2]}
+							\t - **${items[args[2]].protons * args[3]}** Protons
+							\t - **${items[args[2]].electrons * args[3]}** Electrons
+							\t - **${items[args[2]].darkMatter * args[3]}** Dark Matter
+
+							You now have:
+							\t **${userMS.protons}** Protons
+							\t **${userMS.electrons}** Electrons
+							\t **${userMS.darkMatter}** Dark Matter
+						`);
+
+						return msg.channel.send(embed);
+					});
+				} else {
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription('You do not have enough protons/electrons/dark matter to buy this item!');
+
+					return msg.channel.send(embed);
+				}
+			} else if(args[1] === 'sell'){
+				let userMS = await client.utils.loadMsInfo(serverDoc, msg.author.id);
+				if(!args[2]){
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription('Please enter the name of the item you wish to sell!');
+
+					return msg.channel.send(embed);
+				}
+
+				if(!userMS.items.includes(args[2])){
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription('You do not own this item!');
+
+					return msg.channel.send(embed);
+				}
+
+				if(!args[3]) args[3] = 1;
+
+				let protons = items[args[2]].protons * args[3] / 2;
+				let electrons = items[args[2]].electrons * args[3] / 2;
+				let darkMatter = items[args[2]].darkMatter * args[3] / 2;
+
+				let removed = 0;
+				for(let i=0; i < userMS.items.length; i++){
+					if(userMS.items[i] === args[2] && removed <= args[3]){
+						userMS.items.splice(i, 1);
+						removed++;
+					}
+				}
+
+				if(removed < args[3]){
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription('You do not have that many of the specified item!');
+
+					return msg.channel.send(embed);
+				}
+				userMS.protons += protons;
+				userMS.electrons += electrons;
+				userMS.darkMatter += darkMatter;
+				
+				serverDoc.markModified('ms');
+				serverDoc.save().then(() => {
+					const embed = new Discord.MessageEmbed()
+					.setColor(0x000000)
+					.setDescription(`
+						Sell completed!
+
+						\t - **${args[3]}** ${args[2]}
+						\t + **${protons * args[3]}** Protons
+						\t + **${electrons}** Electrons
+						\t + **${darkMatter}** Dark Matter
+
+						You now have:
+						\t **${userMS.protons}** Protons
+						\t **${userMS.electrons}** Electrons
+						\t **${userMS.darkMatter}** Dark Matter
+					`);
+
+					return msg.channel.send(embed);
+				});
+			} else {
+				let itemStr = ""
+				for(let item in items){
+					itemStr = itemStr + `**${item}**: *${items[item].protons} Protons, ${items[item].electrons} Electrons, ${items[item].darkMatter} Dark Matter* \n`
+				}
+				const embed = new Discord.MessageEmbed()
+				.setColor(0x000000)
+				.setTitle(`Singularity Shop`)
+				.setDescription(`
+					${itemStr}
+					*Use \`${serverDoc.prefix}singularity shop buy <item_name> <!quantity>\` to buy an item!*
+				`)
+				.setFooter(`Singularity Shop requested by ${msg.author.tag}`, msg.author.displayAvatarURL());
+
+				return msg.channel.send(embed);
+			}
 		} else {
 			let user = msg.mentions.users.first() ? msg.mentions.users.first() : msg.author;
 			console.log(user.id);
 			let userMS = await client.utils.loadMsInfo(serverDoc, user.id);
 			
-			let itemStr = ''
+			let itemStr = '\n'
 			for(let item of userMS.items){
-				itemStr = itemStr + `- ${item} \n `;
+				itemStr = itemStr + `- **${item}** \n `;
 			}
 
 			let pStr = ''
