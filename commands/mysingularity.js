6509b01c669faf44f327793730cc3626f86431e5
Poly-Pixel <sammelofer@outlook.com>
2021-07-29 20:29:32 -0400

    Change Singularity command to My Singularity command, mysingularity => mysingularity info

--- commands/mysingularity.js
+++ commands/mysingularity.js
@@ -1,28 +1,123 @@
+const prettyMS = require('pretty-ms');
+
+const Shop = require('./singularity/shop/shop.js');
+const Upgrade =  require('./singularity/upgrade.js');
+const Use = require('./singularity/use.js');
+const Info = require('./singularity/info.js');
+
 module.exports = {
     name: 'mysingularity',
-    description: 'Provides information about My Singularity',
+    description: 'Manage your Singularity and view the Singularities of others!',
     type: 'ms',
-    args: [],
+    args: ['!<@user | upgrade | shop>'],
     aliases: ['ms'],
-    example: 'mysingularity',
-    execute(client, Discord, msg, args, serverDoc){
-        const embed = new Discord.MessageEmbed()
-        .setTitle('My Singularity')
-        .setColor(0x000000)
-        .setFooter(`My Singularity info requested by ${msg.author.tag}`, msg.author.displayAvatarURL())
-        .setDescription(`
-            ***My Singularity is the new best way to show off what you've done for a server!***
+    example: 'singularity',
+    async execute(client, Discord, msg, args, serverDoc){
+		const items = [
+			{
+				name: 'Tiny Trophy',
+				protons: 1000000,
+				electrons: 500000,
+				darkMatter: 0
+			},
+			{
+				name: 'Regular Trophy',
+				protons: 10000000,
+				electrons: 1000000,
+				darkMatter: 1
+			},
+			{
+				name: 'Giant Trophy',
+				protons: 1000000000,
+				electrons: 10000000,
+				darkMatter: 5
+			}
+		]
 
-            **What is "My Singularity"?**
-            My Singularity is the feature of this bot that allows you to show off in a whole new way. \
-            Instead of just plain leveling, rank cards, and leaderboards, My Singularity allows you to, hence the name, build *YOUR OWN* black hole!
+		const powerUps = [
+			{
+				name: '2x Proton Boost',
+				protons: 0,
+				electrons: 500,
+				darkMatter: 0,
+				time: 10000
+			},
+			{
+				name: '2x Electron Boost',
+				protons: 5000,
+				electrons: 0,
+				darkMatter: 0,
+				time: 10000
+			}
+		]
 
-            **How does it work?**
-            By being active in a server, you can gain *atoms*. These atoms are a sort of currency in the My Singularity system. \
-            You can use them to make your black hole bigger, or to trade them in for better items. The Developers (poly#3622 and Redstone#1165) will always be adding new features to My Singularity to keep your black hole growing!
+		if(args[0] === 'upgrade'){
+			Upgrade(client, Discord, msg, args, serverDoc);
+		} else if(args[0] === 'shop'){
+			if(args[1] === 'buy'){
+				Shop.buy(client, Discord, msg, args, serverDoc, items, powerUps);
+			} else if(args[1] === 'sell'){
+				Shop.sell(client, Discord, msg, args, serverDoc, items, powerUps);
+			} else {
+				Shop.shop(client, Discord, msg, args, serverDoc, items, powerUps)
+			}
+		} else if(args[0] === 'use'){
+			Use(client, Discord, msg, args, serverDoc, items, powerUps)
+		} else if(args[0] === 'info'){
+			Info(client, Discord, msg, args, serverDoc);
+		} else {
+			let user = msg.mentions.users.first() ? msg.mentions.users.first() : msg.author;
+			if(user.bot){
+				const embed = new Discord.MessageEmbed()
+				.setColor(0x000000)
+				.setDescription('Bots are not powerful enough to have their own Singularity!');
 
-            *Use \`${serverDoc.prefix}help ms\` to get a full list of My Singularity commands*
-        `);
-        msg.channel.send(embed);
-    }
+				return msg.channel.send(embed);
+			}
+			let userMS = await client.utils.loadMsInfo(serverDoc, user.id);
+			
+			let itemStr = '\n'
+			for(let item of userMS.items){
+				itemStr = itemStr + `- **${item}** \n `;
+			}
+			if(itemStr === '\n') itemStr = "**None**";
+
+			let pStr = '\n'
+			for(let p of userMS.powerUps){
+				pStr = pStr + `- **${p}** \n `;
+			}
+			if(pStr === '\n') pStr = '**None**';
+
+			let aStr = '\n'
+			for(let a of userMS.active){
+				aStr = aStr + `- **${a.name}** - ${prettyMS(a.time - (Date.now() - a.start))}** \n `;
+			}
+			if(aStr === '\n') aStr = '**None**';	
+
+			const embed = new Discord.MessageEmbed()
+			.setTitle(`${user.tag}'s Singularity`)
+			.setColor(0x000000)
+			//.setThumbnail('https://cdn.discordapp.com/avatars/835256019336036423/05dde3d48f1a67659be4837607746eb7.webp')
+			.setThumbnail(user.displayAvatarURL())
+			.setDescription(`
+				***------USER STATS------***
+				Protons: **${userMS.protons}**
+				Electrons: **${userMS.electrons}**
+				Dark Matter: **${userMS.darkMatter}**
+				Items: ${itemStr}
+				Power-Ups: ${pStr}
+				Active Power-Ups: ${aStr}
+				Lifetime Experience: **${userMS.lifeExp}**
+
+				***------SINGULARITY STATS------***
+				Singularity Type: **${userMS.singularity.type}**
+				Singularity Size: **${userMS.singularity.size}**
+				Singularity Ferocity: **${userMS.singularity.ferocity}**
+			`)
+			.setFooter(`${user.tag}'s Singularity requested by ${msg.author.tag}`, msg.author.displayAvatarURL());
+
+			msg.channel.send(embed);
+			
+		}
+	}	
 }
---
+++ commands/singularity/info.js
@@ -0,0 +1,20 @@
+module.exports = (client, Discord, msg, args, serverDoc) => {
+	const embed = new Discord.MessageEmbed()
+	.setTitle('My Singularity')
+	.setColor(0x000000)
+	.setFooter(`My Singularity info requested by ${msg.author.tag}`, msg.author.displayAvatarURL())
+	.setDescription(`
+		***My Singularity is the new best way to show off what you've done for a server!***
+
+		**What is "My Singularity"?**
+		My Singularity is the feature of this bot that allows you to show off in a whole new way. \
+		Instead of just plain leveling, rank cards, and leaderboards, My Singularity allows you to, hence the name, build *YOUR OWN* black hole!
+
+		**How does it work?**
+		By being active in a server, you can gain *atoms*. These atoms are a sort of currency in the My Singularity system. \
+		You can use them to make your black hole bigger, or to trade them in for better items. The Developers (poly#3622 and Redstone#1165) will always be adding new features to My Singularity to keep your black hole growing!
+
+		*Use \`${serverDoc.prefix}help ms\` to get a full list of My Singularity commands*
+	`);
+	msg.channel.send(embed);
+}
