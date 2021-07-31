489c929960c0993a609d1989b042fb57f5b979ed
Poly-Pixel <sammelofer@outlook.com>
2021-07-31 18:45:36 -0400

    Add leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,6 +5,31 @@ module.exports = {
 	aliases: ['lb'],
 	example: 'leaderboard',
 	execute(client, Discord, msg, args, serverDoc){
+		let xpArr = [];
+		for(let user of serverDoc.ms){
+			if(user.userID){
+				let member = msg.guild.members.resolve(user.userID);
+				if(member){
+					xpArr.push([member.user.tag, user.lifeExp])
+				}
+			}
+		}
+		let sort = xpArr.sort((a, b) => b[1] - a[1]);
 		
+		let lbStr = ``;
+
+		for(let i=0; i < sort.length; i++){
+			lbStr = lbStr + `**${i + 1}.** **${sort[i][0]}** - **${sort[i][1]} EXP** \n`
+		}
+
+		let currentDate = new Date(Date.now());
+
+		const embed = new Discord.MessageEmbed()
+		.setColor(0x000000)
+		.setTitle(`${msg.guild.name}'s Leaderboard`)
+		.setDescription(lbStr)
+        .setFooter(`${msg.guild.name}'s leaderboard requested by ${msg.author.tag} • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`, msg.author.displayAvatarURL());
+	
+		return msg.channel.send(embed);
 	}
 }
