7ed6cf7cc304f96aedf00f76dcbd703a2f6d1bff
Poly-Pixel <sammelofer@outlook.com>
2021-08-06 19:18:30 -0400

    Fix up clear command & add 'all' option

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports = {
     aliases: ['purge', 'delete'],
     example: 'clear 100',
     execute(client, Discord, msg, args) {
-        if(!msg.member.hasPermission('ADMINISTRATOR')){
+        if(!msg.member.permissions.has('ADMINISTRATOR')){
             const embed = new Discord.MessageEmbed()
             .setDescription('You do not have permission to clear messages!')
             .setColor(0x000000);
@@ -21,10 +21,28 @@ module.exports = {
         } 
 
         if(isNaN(args[0])){
-            const embed = new Discord.MessageEmbed()
-            .setColor(0x000000)
-            .setDescription('Please enter an actual number!');
-            return msg.channel.send({embeds: [embed]});
+            if(args[0] === 'all'){
+                msg.channel.clone({position: msg.channel.rawPosition, reason: 'Clearing channel message history'})
+                .then(newChannel => {
+                    const embed = new Discord.MessageEmbed()
+                    .setColor(0x000000)
+                    .setDescription('Channel message history cleared!');
+
+                    newChannel.send({embeds: [embed]})
+                    .then(sent => {
+                        setTimeout(() => {
+                            sent.delete()
+                        }, 5000)
+                    })
+                })
+                msg.channel.delete({reason: 'Clearing channel message history'})
+                return;
+            } else {
+                const embed = new Discord.MessageEmbed()
+                .setColor(0x000000)
+                .setDescription('Please enter an actual number!');
+                return msg.channel.send({embeds: [embed]});
+            }
         } 
 
         if(args[0] > 100){
@@ -39,28 +57,30 @@ module.exports = {
             .setColor(0x000000)
             .setDescription('You must delete at least one message!');
             return msg.channel.send({embeds: [embed]});
-        } 
+        }
         
-          msg.channel.messages.fetch({limit: Math.floor(args[0])}).then(messages =>{
-             msg.channel.bulkDelete(messages).then(async()=>{
-                 const embed = new Discord.MessageEmbed()
-                 .setDescription(`Successfully cleared \`${args[0]}\` messages!`)
-                 .setColor(0x000000);
-
+        msg.delete().then(() => {
+            msg.channel.bulkDelete(args[0], true).then(async collection => {
+                const embed = new Discord.MessageEmbed()
+                .setDescription(`Successfully cleared \`${collection.size}\` messages!`)
+                .setColor(0x000000);
+    
                 msg.channel.send({embeds: [embed]}).then(sent => {
                     setTimeout(() => {
                         sent.delete();
                     }, 3000);
                 });
-            }, ()=>{
+            })
+
+            .catch(() => {
                 const embed = new Discord.MessageEmbed()
                 .setDescription('At this time, you cannot delete messages that are over 14 days old.')
                 .setColor(0x000000);
                 
                 msg.channel.send({embeds: [embed]});
-
+        
                 return;
             })
-        });
+        })
     }
 }
