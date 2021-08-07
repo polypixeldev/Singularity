cccc47b34d0e029c7ebcf26399d2552b3874032f
Poly-Pixel <sammelofer@outlook.com>
2021-08-06 22:38:14 -0400

    Remove ready.js which for some reason no longer works (transferred to index.js)

--- events/client/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (Discord, client) =>{
-    console.log('Singularity is now online');
-    client.user.setPresence({ activity: { name: 'singularitybot.glitch.me', type: "WATCHING" }, status: 'online' });
-}
