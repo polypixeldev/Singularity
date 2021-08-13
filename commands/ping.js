1d06695f5e3fc6f9d3591bec3df6ee6cf4c88377
Poly-Pixel <sammelofer@outlook.com>
2021-08-12 21:27:17 -0400

    Add "retest" button to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -16,10 +16,59 @@ module.exports = {
     },
     async slashExecute(client, Discord, interaction){
         await interaction.deferReply({ephemeral: true})
-        const embed = new Discord.MessageEmbed()
+        let latestEmbed = new Discord.MessageEmbed()
         .setDescription(`🏓 Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
-        .setColor(0x000000);
+        .setColor(0x000000)
+        .setFooter('get ponged lol');
         
-        interaction.editReply({embeds: [embed]});
+        interaction.editReply({embeds: [latestEmbed], components: [{
+            type: 'ACTION_ROW',
+            components: [{
+                type: 'BUTTON',
+                label: 'Retest',
+                style: 'PRIMARY',
+                customId: 'retest'
+            }]
+        }]}).then(reply => {
+            let collector = reply.createMessageComponentCollector({
+                componentType: 'BUTTON',
+                time: 30000,
+                dispose: true
+            })
+
+            collector.on('collect', async press => {
+                if(press.user.id !== interaction.user.id){
+                    const embed = new Discord.MessageEmbed()
+                    .setColor(0x000000)
+                    .setDescription("You cannot perform this action!")
+
+                    press.reply({embeds: [embed]})
+                } else {
+                    await press.deferUpdate()
+
+                    const embed = new Discord.MessageEmbed()
+                    .setDescription(`🏓 Latency is ${Date.now() - press.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+                    .setColor(0x000000)
+                    .setFooter('get ponged again lol')
+
+                    latestEmbed = embed
+
+                    press.editReply({embeds: [embed]})
+                }
+            })
+
+            collector.on('end', () => {
+                interaction.editReply({components: [{
+                    type: 'ACTION_ROW',
+                    components: [{
+                        type: 'BUTTON',
+                        label: 'Retest',
+                        style: 'PRIMARY',
+                        customId: 'retest',
+                        disabled: true
+                    }]
+                }], embeds: [latestEmbed]})
+            })
+        })
     }
 }
