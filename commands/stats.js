4d38a178a5cda017f5dca4a79289a4470ab1e067
Poly-Pixel <sammelofer@outlook.com>
2021-08-14 19:57:57 -0400

    Add stats command

---
+++ commands/stats.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "stats",
+  description: "Displays statistics about Singularity and the current server",
+  type: "general",
+  options: [],
+  args: [],
+  aliases: [],
+  example: "stats server membercount",
+};
