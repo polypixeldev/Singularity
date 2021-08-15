6d5bc00e0c970d367a20163c870990acddbdc966
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-08-15 20:46:53 +0000

    Fix invite link in links.js

--- commands/links.js
+++ commands/links.js
@@ -8,36 +8,38 @@ module.exports = {
   aliases: ["l"],
   example: "links",
   execute(client, Discord, msg) {
-    let currentDate = new Date(Date.now());
     const embed = new Discord.MessageEmbed()
       .setTitle("Singularity Links")
       .setColor(0x000000);
 
-    msg.reply({ embeds: [embed], components: [
-      {
-        type: "ACTION_ROW",
-        components: [
-          {
-            type: "BUTTON",
-            label: "Bot Invite",
-            url: "https://discord.com/oauth2/authorize?client_id=860552124064202812&permissions=261993005047&redirect_uri=https%3A%2F%2Fsingularitybot.glitch.me%2Flogin&scope=applications.commands%20bot",
-            style: "LINK",
-          },
-          {
-            type: "BUTTON",
-            label: "Support Server",
-            url: "https://discord.gg/Q5GbzpXgSz",
-            style: "LINK",
-          },
-          {
-            type: "BUTTON",
-            label: "GitHub Repository",
-            url: "https://github.com/Poly-Pixel/Singularity",
-            style: "LINK",
-          },
-        ],
-      },
-    ], });
+    msg.reply({
+      embeds: [embed],
+      components: [
+        {
+          type: "ACTION_ROW",
+          components: [
+            {
+              type: "BUTTON",
+              label: "Bot Invite",
+              url: "https://discord.com/oauth2/authorize?client_id=835256019336036423&permissions=261993005047&redirect_uri=https%3A%2F%2Fsingularitybot.glitch.me%2Flogin&scope=applications.commands%20bot",
+              style: "LINK",
+            },
+            {
+              type: "BUTTON",
+              label: "Support Server",
+              url: "https://discord.gg/Q5GbzpXgSz",
+              style: "LINK",
+            },
+            {
+              type: "BUTTON",
+              label: "GitHub Repository",
+              url: "https://github.com/Poly-Pixel/Singularity",
+              style: "LINK",
+            },
+          ],
+        },
+      ],
+    });
   },
   async slashExecute(client, Discord, interaction) {
     await interaction.deferReply({ ephemeral: true });
@@ -55,7 +57,7 @@ module.exports = {
             {
               type: "BUTTON",
               label: "Bot Invite",
-              url: "https://discord.com/oauth2/authorize?client_id=860552124064202812&permissions=261993005047&redirect_uri=https%3A%2F%2Fsingularitybot.glitch.me%2Flogin&scope=applications.commands%20bot",
+              url: "https://discord.com/oauth2/authorize?client_id=835256019336036423&permissions=261993005047&redirect_uri=https%3A%2F%2Fsingularitybot.glitch.me%2Flogin&scope=applications.commands%20bot",
               style: "LINK",
             },
             {
