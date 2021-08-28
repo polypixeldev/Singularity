e24b33fa1762784f9cff25a196832e2c00ac4505
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-08-28 18:20:31 +0000

    enable frontend on express server

--- website/server.js
+++ website/server.js
@@ -51,56 +51,21 @@ class APIClient extends EventEmitter {
   }
 
   startFrontend() {
-    this.app.use(
-      Express.static(
-        "C:/Users/samme/Documents/Programs/Singularity/website/frontend/build"
-      )
-    );
+    this.app.use(Express.static("./frontend/build/"));
     this.app.use((req, res) => {
-      res.sendFile("../frontend/build/index.html");
+      res.sendFile(__dirname + "./frontend/build/index.html");
     });
   }
 
   startFull() {
-    this.app.use(
-      Express.static(
-        "C:/Users/samme/Documents/Programs/Singularity/website/frontend/build"
-      )
-    );
+    this.app.use(Express.static(__dirname + "/frontend/build/"));
+
     this.app.use("/api", apiRouter(this));
+
     this.app.use((req, res) => {
-      res.sendFile(
-        "C:/Users/samme/Documents/Programs/Singularity/website/frontend/build/index.html"
-      );
+      res.sendFile(__dirname + "/frontend/build/index.html");
     });
   }
 }
 
 module.exports = APIClient;
-// module.exports = {
-// 	start(type){
-// 		const app = Express();
-
-// 		if(type === 'frontend'){
-// 			app.use(Express.static('C:/Users/samme/Documents/Programs/Singularity/website/frontend/build'));
-// 			app.use((req, res) => {
-// 				res.sendFile('../frontend/build/index.html');
-// 			})
-// 		} else if(type === 'backend'){
-// 			app.use('/api', apiRouter)
-// 		} else {
-// 			app.use(Express.static('C:/Users/samme/Documents/Programs/Singularity/website/frontend/build'));
-// 			app.get('/api/', (req, res) => {
-// 				res.send('api');
-// 			})
-// 			app.use((req, res) => {
-// 				res.sendFile('C:/Users/samme/Documents/Programs/Singularity/website/frontend/build/index.html');
-// 			})
-// 		}
-
-// 		app.listen(5000, 'localhost', () => {
-// 			console.log(`${type === 'frontend' ? 'Frontend': type ===  'backend' ? 'Backend' : 'Full'} Server running at http://localhost:5000`);
-// 		})
-
-// 	}
-// }
