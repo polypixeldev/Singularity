cf02ef8398e810f6e6f34fb19831c2d7061e15ec
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-05 13:15:30 -0400

    pass serverdoc to mute/unmute context menu interaction

--- contexts/mute-user.js
+++ contexts/mute-user.js
@@ -1,7 +1,12 @@
 module.exports = {
   name: "Mute User",
   type: "USER",
-  execute(client, Discord, interaction) {
-    require("../commands/mute.js").slashExecute(client, Discord, interaction);
+  execute(client, Discord, interaction, serverDoc) {
+    require("../commands/mute.js").slashExecute(
+      client,
+      Discord,
+      interaction,
+      serverDoc
+    );
   },
 };
--- contexts/unmute-user.js
+++ contexts/unmute-user.js
@@ -1,7 +1,12 @@
 module.exports = {
   name: "Unmute User",
   type: "USER",
-  execute(client, Discord, interaction) {
-    require("../commands/unmute.js").slashExecute(client, Discord, interaction);
+  execute(client, Discord, interaction, serverDoc) {
+    require("../commands/unmute.js").slashExecute(
+      client,
+      Discord,
+      interaction,
+      serverDoc
+    );
   },
 };
