bdeb0121cb70c436f90c9fcc5efb0007d5c8382e
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-06 14:38:35 -0400

    select menu for upgrade.js

--- commands/ms/upgrade.js
+++ commands/ms/upgrade.js
@@ -108,7 +108,7 @@ module.exports = {
   },
   async slashExecute(client, Discord, interaction, serverDoc) {
     await interaction.deferReply({ ephemeral: true });
-    let userMS = await client.utils.loadMsInfo(
+    let userMS = await client.utils.loadUserInfo(
       client,
       serverDoc,
       interaction.user.id
@@ -156,55 +156,104 @@ module.exports = {
         } • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`,
         interaction.user.displayAvatarURL()
       );
-    interaction.editReply({ embeds: [embed] });
+    interaction
+      .editReply({
+        embeds: [embed],
+        components: [
+          {
+            type: "ACTION_ROW",
+            components: [
+              {
+                type: "SELECT_MENU",
+                label: "Upgrade Quantity",
+                custom_id: "quantity",
+                options: [
+                  {
+                    label: "1x Upgrade",
+                    value: "1",
+                    description: "Upgrade your Singularity once",
+                  },
+                  {
+                    label: "2x Upgrade",
+                    value: "2",
+                    description: "Upgrade your Singularity twice",
+                  },
+                  {
+                    label: "3x Upgrade",
+                    value: "3",
+                    description: "Upgrade your Singularity thrice",
+                  },
+                  {
+                    label: "5x Upgrade",
+                    value: "5",
+                    description: "Upgrade your Singularity 5 times",
+                  },
+                  {
+                    label: "10x Upgrade",
+                    value: "10",
+                    description: "Upgrade your Singularity 10 times",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      })
+      .then((sent) => {
+        sent
+          .awaitMessageComponent({
+            filter: (inter) => inter.user.id === interaction.user.id,
+            time: 30000,
+            componentType: "SELECT_MENU",
+          })
+          .then(async (selectionInteraction) => {
+            await selectionInteraction.deferUpdate();
+            let num = Number(selectionInteraction.values[0]);
 
-    interaction.channel
-      .awaitMessages(
-        (message) =>
-          message.author.id === interaction.user.id &&
-          Number.isInteger(Number(message.content)),
-        { max: 1, time: 30000, errors: ["time"] }
-      )
-      .then((collection) => {
-        let num = Number(collection.first().content);
-        if (num > limit) {
-          const embed = new Discord.MessageEmbed()
-            .setColor(0x000000)
-            .setDescription(
-              "You do not have enough protons/electrons to upgrade your Singularity size this much!"
-            );
-
-          return interaction.followUp({ embeds: [embed], ephemeral: true });
-        } else if (num <= 0) {
-          const embed = new Discord.MessageEmbed()
-            .setColor(0x000000)
-            .setDescription("Upgrade Aborted.");
-
-          return interaction.followUp({ embeds: [embed], ephemeral: true });
-        } else {
-          userMS.electrons -= num * 250;
-          userMS.protons -= num * 1000;
-          userMS.electrons -= userMS.singularity.size * 7;
-          userMS.protons -= userMS.singularity.size * 25;
-          userMS.singularity.size += Math.floor(Math.random() * 9 + 1);
-          client.utils
-            .updateUser(client, serverDoc.guildID, userMS.userID, userMS)
-            .then(() => {
+            if (num > limit) {
               const embed = new Discord.MessageEmbed()
                 .setColor(0x000000)
                 .setDescription(
-                  `Congrats! Your Singularity is now size \`${userMS.singularity.size}\`!`
+                  "You do not have enough protons/electrons to upgrade your Singularity size this much!"
                 );
 
               return interaction.followUp({ embeds: [embed], ephemeral: true });
-            });
-        }
-      })
-      .catch(() => {
-        const embed = new Discord.MessageEmbed()
-          .setColor(0x000000)
-          .setDescription("You did not respond with a valid number in time.");
-        return interaction.followUp({ embeds: [embed], ephemeral: true });
+            } else if (num <= 0) {
+              const embed = new Discord.MessageEmbed()
+                .setColor(0x000000)
+                .setDescription("Upgrade Aborted.");
+
+              return interaction.followUp({ embeds: [embed], ephemeral: true });
+            } else {
+              userMS.electrons -= num * 250;
+              userMS.protons -= num * 1000;
+              userMS.electrons -= userMS.singularity.size * 7;
+              userMS.protons -= userMS.singularity.size * 25;
+              userMS.singularity.size += Math.floor(Math.random() * 9 + 1);
+              client.utils
+                .updateUser(client, serverDoc.guildID, userMS.userID, userMS)
+                .then(() => {
+                  const embed = new Discord.MessageEmbed()
+                    .setColor(0x000000)
+                    .setDescription(
+                      `Congrats! Your Singularity is now size \`${userMS.singularity.size}\`!`
+                    );
+
+                  return interaction.followUp({
+                    embeds: [embed],
+                    ephemeral: true,
+                  });
+                });
+            }
+          })
+          .catch(() => {
+            const embed = new Discord.MessageEmbed()
+              .setColor(0x000000)
+              .setDescription(
+                "You did not respond with a valid number in time."
+              );
+            return interaction.followUp({ embeds: [embed], ephemeral: true });
+          });
       });
   },
 };
