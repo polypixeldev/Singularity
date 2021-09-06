d476dec97ef5166066234795eed2f6dc6aa83013
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-06 14:42:23 -0400

    fix rare.js

--- commands/ms/rare.js
+++ commands/ms/rare.js
@@ -6,28 +6,11 @@ module.exports = {
   aliases: [],
   example: "ms rare",
   options: [],
-  execute(client, Discord, msg, args, serverDoc, rareItems) {
-    let currentDate = new Date(Date.now());
-    const embed = new Discord.MessageEmbed()
-      .setColor(0x000000)
-      .setTitle(`My Singularity - Rare Items`)
-      .setDescription(
-        "These items are **rare**, and cannot be bought from the Singularity shop. Instead, you have a chance to find them when doing special actions, such as prestiging."
-      )
-      .setFooter(
-        `Rare item list requested by ${
-          msg.author.tag
-        } • ${currentDate.getUTCMonth()}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()} @ ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()} UTC`
-      );
-    for (let item of rareItems) {
-      embed.addField(item.name, item.description);
-    }
-
-    msg.channel.send({ embeds: [embed] });
-  },
-  async slashExecute(client, Discord, interaction, serverDoc, rareItems) {
+  async slashExecute(client, Discord, interaction, serverDoc) {
     await interaction.deferReply({ ephemeral: true });
     let currentDate = new Date(Date.now());
+
+    const rareItems = serverDoc.items.filter((item) => item.rare === true);
     const embed = new Discord.MessageEmbed()
       .setColor(0x000000)
       .setTitle(`My Singularity - Rare Items`)
