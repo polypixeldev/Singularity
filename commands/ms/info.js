9d45cb88deacb7f2a7f3afc94d088adf169e83e5
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-08 17:52:43 -0400

    radiation & formatting for info.js

--- commands/ms/info.js
+++ commands/ms/info.js
@@ -37,14 +37,28 @@ module.exports = {
           value: `
         By being active in a server, you can gain *protons* and *electrons*. These protons and electrons are a sort of currency in the My Singularity system. \
         You can use them to make your black hole bigger, or to trade them in for better items. The Developers (poly#3622 and Redstone#1165) will always be adding new features to My Singularity to keep your black hole growing!
-
-        In addition to protons and electrons, you may get *dark matter*. This is an extremely rare substance, and can only be acquired naturally in 2 ways:
-          
-          **1.** While chatting, a unique and rare phenomenon called "quantum tunneling" may occur. When this occurs, you will recieve 1 Dark Matter.
-          **2.** Once daily, you may "explore" to get a chance at getting dark matter. More information on this feature is in \`${serverDoc.prefix}ms explore\`.
-          
+        
         *Use \`${serverDoc.prefix}help ms\` to get a full list of My Singularity commands*
       `,
+          inline: false,
+        },
+        {
+          name: "Dark Matter",
+          value: `
+            In addition to protons and electrons, you may get *dark matter*. This is an extremely rare substance, and can only be acquired naturally in 2 ways:
+            
+            **1.** While chatting, a unique and rare phenomenon called "quantum tunneling" may occur. When this occurs, you will recieve 1 Dark Matter.
+            **2.** Once daily, you may "explore" to get a chance at getting dark matter. More information on this feature is in \`${serverDoc.prefix}ms explore\`.
+          `,
+          inline: false,
+        },
+        {
+          name: "Additional Features",
+          value: `
+            However, your Singularity won't last forever. When you become inactive and generate no protons or electrons for your Singularity, it will start to radiate away.
+            This process is slow, and won't start the moment you don't log on for a week. Nevertheless, after a period of inactivity, your protons and electrons will begin to fade away.
+            Note that your Lifetime Experience is not affected by this process.
+          `,
           inline: false,
         }
       );
