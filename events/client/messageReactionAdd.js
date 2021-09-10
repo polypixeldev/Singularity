bc72bebc8925c3137d7273500a536b070a2a1901
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-10 18:11:14 -0400

    Unnecessary variables in messageReactionAdd

--- events/client/messageReactionAdd.js
+++ events/client/messageReactionAdd.js
@@ -9,23 +9,21 @@ module.exports = async (Discord, client, reaction, user) => {
       console.log("Something went wrong when fetching the message: ", error);
     });
   }
-  let realReaction = reaction;
-  let realUser = user;
   const serverDoc = await client.utils.loadGuildInfo(
     client,
     reaction.message.guild
   );
   const roleListener = serverDoc.reactionRoles;
-  //console.log(`Reaction added: ${realReaction.emoji.name} on ${realReaction.message.content} by ${realReaction.message.author.tag} in ${realReaction.message.channel} in ${realReaction.message.channel.guild}`);
+  //console.log(`Reaction added: ${reaction.emoji.name} on ${reaction.message.content} by ${reaction.message.author.tag} in ${reaction.message.channel} in ${reaction.message.channel.guild}`);
   for (let i = 0; i < roleListener.length; i++) {
     if (
-      realReaction.message.id === roleListener[i][2] &&
-      realUser.tag !== "Singularity#9601"
+      reaction.message.id === roleListener[i][2] &&
+      user.tag !== "Singularity#9601"
     ) {
-      if (realReaction.emoji.name === roleListener[i][1]) {
-        const member = realReaction.message.guild.members.resolve(realUser);
-        await realReaction.message.guild.roles.fetch();
-        let roleToAddToMember = realReaction.message.guild.roles.cache.find(
+      if (reaction.emoji.name === roleListener[i][1]) {
+        const member = reaction.message.guild.members.resolve(user);
+        await reaction.message.guild.roles.fetch();
+        let roleToAddToMember = reaction.message.guild.roles.cache.find(
           (role) => role.name === roleListener[i][0]
         );
         member.roles.add(roleToAddToMember.id);
