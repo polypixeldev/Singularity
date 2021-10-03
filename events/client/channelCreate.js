5cc7a62f0d3479c9523b30b47df6d5823a5f3433
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-03 11:06:38 -0400

    add channelCreate handler for mute role overrides

---
+++ events/client/channelCreate.js
@@ -0,0 +1,33 @@
+module.exports = async (Discord, client, channel) => {
+  await channel.guild.roles.fetch();
+
+  let muteRole = channel.guild.roles.cache.find((rl) => rl.name === "Muted");
+
+  if (!muteRole) return;
+
+  if (!(channel instanceof Discord.ThreadChannel)) {
+    if (channel.manageable) {
+      if (channel.isText()) {
+        channel.permissionOverwrites.create(
+          muteRole,
+          {
+            SEND_MESSAGES: false,
+          },
+          {
+            reason: "Setting up Muted role",
+          }
+        );
+      } else {
+        channel.permissionOverwrites.create(
+          muteRole,
+          {
+            SPEAK: false,
+          },
+          {
+            reason: "Setting up Muted role",
+          }
+        );
+      }
+    }
+  }
+};
