9c92930af481e39375cd53298ff1f05da06c3ad6
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2022-04-24 16:43:21 -0400

    Bump version numbers

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ dotenv.config();
 
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
-	release: "Singularity@1.0.3",
+	release: "Singularity@2.0.0",
 	tracesSampleRate: 1.0,
 	integrations: [
 		new Sentry.Integrations.Http({ tracing: true }),
